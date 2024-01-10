aecbdff53d68e47e38a00c77bf82dec8057bb5d1
Abhishek Sharma <sharma312006@gmail.com>
2024-01-10 17:25:51 +0530

    midleware start

---
+++ ArrayMethod/index.js
@@ -0,0 +1,16 @@
+//map filter arrow function
+
+const sum = (a, b) => {
+  return a + b;
+};
+const ans = sum(1, 2);
+console.log(ans);
+
+//map
+//2,4,6,8,10
+const arr = [1, 2, 3, 4, 5];
+
+const output = arr.map((data) => {
+  return data * 2;
+});
+console.log(output);
